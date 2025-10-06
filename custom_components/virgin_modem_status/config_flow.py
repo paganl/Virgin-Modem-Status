c1e2247dbd8e81f8d509ea7407e48be9823f8ba0
Paul Gale <paul.gale@pagabv.com>
2025-10-06 17:23:58 +0100

    Update config_flow.py

--- custom_components/virgin_modem_status/config_flow.py
+++ custom_components/virgin_modem_status/config_flow.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN, DEFAULT_HOST, DEFAULT_SCAN_INTERVAL
-from .api import VirginApi
+from .const import DOMAIN, DEFAULT_HOST
+from .api import VirginApi, VirginApiError
 
 STEP_USER = vol.Schema({
     vol.Optional("host", default=DEFAULT_HOST): str,
@@ -22,29 +21,17 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         host = user_input["host"]
         # quick connectivity check
         api = VirginApi(host, async_get_clientsession(self.hass))
+        errors = {}
         try:
-            await api.get_status()
-        except Exception as e:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER,
-                errors={"base": "cannot_connect"},
-                description_placeholders={"err": str(e)}
-            )
+            await api.fetch_snapshot()
+        except VirginApiError:
+            errors["host"] = "cannot_connect"
 
-        return self.async_create_entry(title=f"Virgin Modem ({host})", data={"host": host})
+        if errors:
+            return self.async_show_form(step_id="user", data_schema=STEP_USER, errors=errors)
 
-    async def async_step_import(self, data):
-        return await self.async_step_user(data)
+        return self.async_create_entry(title="Virgin Modem Status", data={"host": host})
 
-class OptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        schema = vol.Schema({
-            vol.Optional("scan_interval", default=self.config_entry.options.get("scan_interval", DEFAULT_SCAN_INTERVAL)): int
-        })
-        if user_input is None:
-            return self.async_show_form(step_id="init", data_schema=schema)
-        return self.async_create_entry(title="", data=user_input)
+    async def async_step_import(self, user_input: dict) -> FlowResult:
+        # Optional: support YAML import
+        return await self.async_step_user(user_input)
